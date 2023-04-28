520d8a1f467c38016368c1b91d328287a46b76ec
ugsankun <95005131+ugsankun@users.noreply.github.com>
2023-04-28 22:16:54 +0900

    1

--- main.ts
+++ main.ts
@@ -1,26 +1,28 @@
-input.onLogoEvent(TouchButtonEvent.Touched, function () {
-	
-})
 input.onLogoEvent(TouchButtonEvent.LongPressed, function () {
 	
 })
 input.onButtonPressed(Button.A, function () {
-    pins.digitalWritePin(DigitalPin.P0, 1)
-    pins.digitalWritePin(DigitalPin.P1, 0)
-    basic.pause(2215)
-    pins.digitalWritePin(DigitalPin.P0, 0)
-    pins.digitalWritePin(DigitalPin.P1, 0)
+	
 })
 input.onButtonPressed(Button.B, function () {
-    pins.digitalWritePin(DigitalPin.P1, 1)
+    pins.digitalWritePin(DigitalPin.P13, 1)
+    pins.digitalWritePin(DigitalPin.P14, 0)
+    basic.pause(4400)
+    pins.digitalWritePin(DigitalPin.P13, 0)
+    pins.digitalWritePin(DigitalPin.P14, 0)
+    basic.pause(1000)
+    pins.digitalWritePin(DigitalPin.P0, 1)
+    pins.digitalWritePin(DigitalPin.P1, 0)
+    basic.pause(1200)
     pins.digitalWritePin(DigitalPin.P0, 0)
-    basic.pause(2300)
+    pins.digitalWritePin(DigitalPin.P1, 1)
+    basic.pause(1120)
     pins.digitalWritePin(DigitalPin.P0, 0)
     pins.digitalWritePin(DigitalPin.P1, 0)
-})
-basic.forever(function () {
-    if (input.logoIsPressed()) {
-        pins.digitalWritePin(DigitalPin.P0, 1)
-        pins.digitalWritePin(DigitalPin.P1, 0)
-    }
+    basic.pause(1000)
+    pins.digitalWritePin(DigitalPin.P13, 0)
+    pins.digitalWritePin(DigitalPin.P14, 1)
+    basic.pause(4400)
+    pins.digitalWritePin(DigitalPin.P13, 0)
+    pins.digitalWritePin(DigitalPin.P14, 0)
 })
