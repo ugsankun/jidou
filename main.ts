1e62ed6b48d2e78d2cd979d33fac78aa1a688098
ugsankun <95005131+ugsankun@users.noreply.github.com>
2023-04-29 17:49:55 +0900

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,14 +1,15 @@
 input.onButtonPressed(Button.A, function () {
-    pins.analogWritePin(AnalogPin.P13, 0)
-    pins.analogWritePin(AnalogPin.P14, 437)
+    pins.analogWritePin(AnalogPin.P13, 470)
+    pins.analogWritePin(AnalogPin.P14, 0)
     basic.pause(200)
 })
 input.onButtonPressed(Button.B, function () {
-    pins.digitalWritePin(DigitalPin.P13, 1)
-    pins.digitalWritePin(DigitalPin.P14, 0)
-    basic.pause(4400)
-    pins.digitalWritePin(DigitalPin.P13, 0)
-    pins.digitalWritePin(DigitalPin.P14, 0)
+    isB = 1
+    pins.digitalWritePin(DigitalPin.P8, 0)
+    pins.digitalWritePin(DigitalPin.P9, 1)
+    basic.pause(4800)
+    pins.digitalWritePin(DigitalPin.P8, 0)
+    pins.digitalWritePin(DigitalPin.P9, 0)
     basic.pause(1000)
     pins.digitalWritePin(DigitalPin.P0, 1)
     pins.digitalWritePin(DigitalPin.P1, 0)
@@ -19,30 +20,28 @@ input.onButtonPressed(Button.B, function () {
     pins.digitalWritePin(DigitalPin.P0, 0)
     pins.digitalWritePin(DigitalPin.P1, 0)
     basic.pause(1000)
-    pins.digitalWritePin(DigitalPin.P13, 0)
-    pins.digitalWritePin(DigitalPin.P14, 1)
-    basic.pause(4300)
-    pins.digitalWritePin(DigitalPin.P13, 0)
-    pins.digitalWritePin(DigitalPin.P14, 0)
+    pins.digitalWritePin(DigitalPin.P8, 1)
+    pins.digitalWritePin(DigitalPin.P9, 0)
+    basic.pause(4750)
+    pins.digitalWritePin(DigitalPin.P8, 0)
+    pins.digitalWritePin(DigitalPin.P9, 0)
+    isB = 0
 })
-pins.setPull(DigitalPin.P2, PinPullMode.PullDown)
-pins.setPull(DigitalPin.P6, PinPullMode.PullDown)
+let isB = 0
+pins.setPull(DigitalPin.P15, PinPullMode.PullDown)
+pins.setPull(DigitalPin.P16, PinPullMode.PullDown)
+isB = 0
 basic.forever(function () {
-    if (pins.digitalReadPin(DigitalPin.P2) == 1) {
-        pins.analogWritePin(AnalogPin.P13, 330)
-        pins.analogWritePin(AnalogPin.P14, 0)
+    if (pins.digitalReadPin(DigitalPin.P16) == 1) {
+        pins.analogWritePin(AnalogPin.P8, 0)
+        pins.analogWritePin(AnalogPin.P9, 500)
+    } else if (pins.digitalReadPin(DigitalPin.P15) == 1) {
+        pins.analogWritePin(AnalogPin.P8, 500)
+        pins.analogWritePin(AnalogPin.P9, 0)
     } else {
-        pins.analogWritePin(AnalogPin.P13, 0)
-        pins.analogWritePin(AnalogPin.P14, 0)
+        if (isB == 0) {
+            pins.analogWritePin(AnalogPin.P8, 0)
+            pins.analogWritePin(AnalogPin.P9, 0)
+        }
     }
-    if (pins.digitalReadPin(DigitalPin.P2) == 1) {
-        pins.analogWritePin(AnalogPin.P13, 0)
-        pins.analogWritePin(AnalogPin.P14, 272)
-    } else {
-        pins.analogWritePin(AnalogPin.P13, 0)
-        pins.analogWritePin(AnalogPin.P14, 0)
-    }
-})
-basic.forever(function () {
-	
 })
