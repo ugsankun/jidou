6d217e700dd875c73d6a433ecbe0dc99bbd58067
ugsankun <95005131+ugsankun@users.noreply.github.com>
2023-04-29 13:29:13 +0900

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,7 @@
-input.onLogoEvent(TouchButtonEvent.LongPressed, function () {
-	
-})
 input.onButtonPressed(Button.A, function () {
-	
+    pins.analogWritePin(AnalogPin.P13, 0)
+    pins.analogWritePin(AnalogPin.P14, 437)
+    basic.pause(200)
 })
 input.onButtonPressed(Button.B, function () {
     pins.digitalWritePin(DigitalPin.P13, 1)
@@ -22,7 +21,28 @@ input.onButtonPressed(Button.B, function () {
     basic.pause(1000)
     pins.digitalWritePin(DigitalPin.P13, 0)
     pins.digitalWritePin(DigitalPin.P14, 1)
-    basic.pause(4400)
+    basic.pause(4300)
     pins.digitalWritePin(DigitalPin.P13, 0)
     pins.digitalWritePin(DigitalPin.P14, 0)
+})
+pins.setPull(DigitalPin.P2, PinPullMode.PullDown)
+pins.setPull(DigitalPin.P6, PinPullMode.PullDown)
+basic.forever(function () {
+    if (pins.digitalReadPin(DigitalPin.P2) == 1) {
+        pins.analogWritePin(AnalogPin.P13, 330)
+        pins.analogWritePin(AnalogPin.P14, 0)
+    } else {
+        pins.analogWritePin(AnalogPin.P13, 0)
+        pins.analogWritePin(AnalogPin.P14, 0)
+    }
+    if (pins.digitalReadPin(DigitalPin.P2) == 1) {
+        pins.analogWritePin(AnalogPin.P13, 0)
+        pins.analogWritePin(AnalogPin.P14, 272)
+    } else {
+        pins.analogWritePin(AnalogPin.P13, 0)
+        pins.analogWritePin(AnalogPin.P14, 0)
+    }
+})
+basic.forever(function () {
+	
 })
