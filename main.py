33afdd2f222c6b6e080f0ab2ad299970f4f93ac9
ugsankun <95005131+ugsankun@users.noreply.github.com>
2023-05-01 20:18:58 +0900

    更新 main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,51 +1,150 @@
-def on_button_pressed_a():
-    music.play_tone(262, music.beat(BeatFraction.WHOLE))
-    pins.digital_write_pin(DigitalPin.P7, 1)
-    pins.digital_write_pin(DigitalPin.P6, 0)
-    basic.pause(500)
-    pins.digital_write_pin(DigitalPin.P6, 0)
-    pins.digital_write_pin(DigitalPin.P7, 0)
-input.on_button_pressed(Button.A, on_button_pressed_a)
-
-def on_button_pressed_b():
+# keypad
+# 
+# pin8
+# 
+# pin16
+# 
+# pin13
+# 
+# pin14
+# 
+# pin0
+# 
+# pin1
+# 
+# pin2
+# 
+# motor
+# 
+# pin5
+# 
+# pin11
+# 
+# pin12
+# 
+# pin15
+def ame1():
     global isB
     isB = 1
-    pins.digital_write_pin(DigitalPin.P8, 0)
-    pins.digital_write_pin(DigitalPin.P9, 1)
-    basic.pause(4800)
-    pins.digital_write_pin(DigitalPin.P8, 0)
-    pins.digital_write_pin(DigitalPin.P9, 0)
+    pins.digital_write_pin(DigitalPin.P5, 0)
+    pins.digital_write_pin(DigitalPin.P11, 1)
+    basic.pause(3600)
+    pins.digital_write_pin(DigitalPin.P5, 0)
+    pins.digital_write_pin(DigitalPin.P11, 0)
     basic.pause(500)
-    pins.digital_write_pin(DigitalPin.P6, 1)
-    pins.digital_write_pin(DigitalPin.P7, 0)
+    pins.digital_write_pin(DigitalPin.P12, 1)
+    pins.digital_write_pin(DigitalPin.P15, 0)
+    basic.pause(1300)
+    pins.digital_write_pin(DigitalPin.P12, 0)
+    pins.digital_write_pin(DigitalPin.P15, 1)
+    basic.pause(1200)
+    pins.digital_write_pin(DigitalPin.P12, 0)
+    pins.digital_write_pin(DigitalPin.P15, 0)
     basic.pause(500)
-    pins.digital_write_pin(DigitalPin.P6, 0)
-    pins.digital_write_pin(DigitalPin.P7, 1)
-    basic.pause(500)
-    pins.digital_write_pin(DigitalPin.P6, 0)
-    pins.digital_write_pin(DigitalPin.P7, 0)
-    basic.pause(500)
-    pins.digital_write_pin(DigitalPin.P8, 1)
-    pins.digital_write_pin(DigitalPin.P9, 0)
-    basic.pause(4750)
-    pins.digital_write_pin(DigitalPin.P8, 0)
-    pins.digital_write_pin(DigitalPin.P9, 0)
+    pins.digital_write_pin(DigitalPin.P5, 1)
+    pins.digital_write_pin(DigitalPin.P11, 0)
+    basic.pause(3550)
+    pins.digital_write_pin(DigitalPin.P5, 0)
+    pins.digital_write_pin(DigitalPin.P11, 0)
     isB = 0
-input.on_button_pressed(Button.B, on_button_pressed_b)
-
+def ame2():
+    global isB
+    isB = 1
+    pins.digital_write_pin(DigitalPin.P5, 0)
+    pins.digital_write_pin(DigitalPin.P11, 1)
+    basic.pause(4800)
+    pins.digital_write_pin(DigitalPin.P5, 0)
+    pins.digital_write_pin(DigitalPin.P11, 0)
+    basic.pause(500)
+    pins.digital_write_pin(DigitalPin.P12, 1)
+    pins.digital_write_pin(DigitalPin.P15, 0)
+    basic.pause(1300)
+    pins.digital_write_pin(DigitalPin.P12, 0)
+    pins.digital_write_pin(DigitalPin.P15, 1)
+    basic.pause(1200)
+    pins.digital_write_pin(DigitalPin.P12, 0)
+    pins.digital_write_pin(DigitalPin.P15, 0)
+    basic.pause(500)
+    pins.digital_write_pin(DigitalPin.P5, 1)
+    pins.digital_write_pin(DigitalPin.P11, 0)
+    basic.pause(4750)
+    pins.digital_write_pin(DigitalPin.P5, 0)
+    pins.digital_write_pin(DigitalPin.P11, 0)
+    isB = 0
 isB = 0
-pins.set_pull(DigitalPin.P15, PinPullMode.PULL_DOWN)
-pins.set_pull(DigitalPin.P16, PinPullMode.PULL_DOWN)
 isB = 0
 
 def on_forever():
+    pins.digital_write_pin(DigitalPin.P8, 0)
+    pins.digital_write_pin(DigitalPin.P0, 1)
     if pins.digital_read_pin(DigitalPin.P16) == 1:
-        pins.analog_write_pin(AnalogPin.P8, 0)
-        pins.analog_write_pin(AnalogPin.P9, 500)
-    elif pins.digital_read_pin(DigitalPin.P15) == 1:
-        pins.analog_write_pin(AnalogPin.P8, 500)
-        pins.analog_write_pin(AnalogPin.P9, 0)
-    elif isB == 0:
-        pins.analog_write_pin(AnalogPin.P8, 0)
-        pins.analog_write_pin(AnalogPin.P9, 0)
+        basic.show_string("c")
+        pins.analog_write_pin(AnalogPin.P5, 0)
+        pins.analog_write_pin(AnalogPin.P11, 500)
+    elif pins.digital_read_pin(DigitalPin.P13) == 1:
+        basic.show_number(0)
+    elif pins.digital_read_pin(DigitalPin.P14) == 1:
+        basic.show_string(".")
+        pins.analog_write_pin(AnalogPin.P5, 500)
+        pins.analog_write_pin(AnalogPin.P11, 0)
+    else:
+        basic.show_leds("""
+            . . . . .
+                        . . . . .
+                        . . . . .
+                        . . . . .
+                        . . . . .
+        """)
+        pins.analog_write_pin(AnalogPin.P5, 0)
+        pins.analog_write_pin(AnalogPin.P11, 0)
+    pins.digital_write_pin(DigitalPin.P0, 0)
+    pins.digital_write_pin(DigitalPin.P1, 1)
+    if pins.digital_read_pin(DigitalPin.P16) == 1:
+        basic.show_number(1)
+        ame2()
+    elif pins.digital_read_pin(DigitalPin.P13) == 1:
+        basic.show_number(2)
+        ame2()
+    elif pins.digital_read_pin(DigitalPin.P14) == 1:
+        basic.show_number(3)
+    else:
+        basic.show_leds("""
+            . . . . .
+                        . . . . .
+                        . . . . .
+                        . . . . .
+                        . . . . .
+        """)
+    pins.digital_write_pin(DigitalPin.P1, 0)
+    pins.digital_write_pin(DigitalPin.P2, 1)
+    if pins.digital_read_pin(DigitalPin.P16) == 1:
+        basic.show_number(4)
+    elif pins.digital_read_pin(DigitalPin.P13) == 1:
+        basic.show_number(5)
+    elif pins.digital_read_pin(DigitalPin.P14) == 1:
+        basic.show_number(6)
+    else:
+        basic.show_leds("""
+            . . . . .
+                        . . . . .
+                        . . . . .
+                        . . . . .
+                        . . . . .
+        """)
+    pins.digital_write_pin(DigitalPin.P2, 0)
+    pins.digital_write_pin(DigitalPin.P8, 1)
+    if pins.digital_read_pin(DigitalPin.P16) == 1:
+        basic.show_number(7)
+    elif pins.digital_read_pin(DigitalPin.P13) == 1:
+        basic.show_number(8)
+    elif pins.digital_read_pin(DigitalPin.P14) == 1:
+        basic.show_number(9)
+    else:
+        basic.show_leds("""
+            . . . . .
+                        . . . . .
+                        . . . . .
+                        . . . . .
+                        . . . . .
+        """)
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,37 +1,35 @@
-/**
- * keypad
- * 
- * pin8
- * 
- * pin16
- * 
- * pin13
- * 
- * pin14
- * 
- * pin0
- * 
- * pin1
- * 
- * pin2
- * 
- * motor
- * 
- * pin5
- * 
- * pin11
- * 
- * pin12
- * 
- * pin15
- */
+// keypad
+// 
+// pin8
+// 
+// pin16
+// 
+// pin13
+// 
+// pin14
+// 
+// pin0
+// 
+// pin1
+// 
+// pin2
+// 
+// motor
+// 
+// pin5
+// 
+// pin11
+// 
+// pin12
+// 
+// pin15
 function ame1 () {
     isB = 1
-    pins.digitalWritePin(DigitalPin.P5, 0)
-    pins.digitalWritePin(DigitalPin.P11, 1)
+    pins.digitalWritePin(DigitalPin.P9, 0)
+    pins.digitalWritePin(DigitalPin.P10, 1)
     basic.pause(3600)
-    pins.digitalWritePin(DigitalPin.P5, 0)
-    pins.digitalWritePin(DigitalPin.P11, 0)
+    pins.digitalWritePin(DigitalPin.P9, 0)
+    pins.digitalWritePin(DigitalPin.P10, 0)
     basic.pause(500)
     pins.digitalWritePin(DigitalPin.P12, 1)
     pins.digitalWritePin(DigitalPin.P15, 0)
@@ -42,20 +40,20 @@ function ame1 () {
     pins.digitalWritePin(DigitalPin.P12, 0)
     pins.digitalWritePin(DigitalPin.P15, 0)
     basic.pause(500)
-    pins.digitalWritePin(DigitalPin.P5, 1)
-    pins.digitalWritePin(DigitalPin.P11, 0)
+    pins.digitalWritePin(DigitalPin.P9, 1)
+    pins.digitalWritePin(DigitalPin.P10, 0)
     basic.pause(3550)
-    pins.digitalWritePin(DigitalPin.P5, 0)
-    pins.digitalWritePin(DigitalPin.P11, 0)
+    pins.digitalWritePin(DigitalPin.P9, 0)
+    pins.digitalWritePin(DigitalPin.P10, 0)
     isB = 0
 }
 function ame2 () {
     isB = 1
-    pins.digitalWritePin(DigitalPin.P5, 0)
-    pins.digitalWritePin(DigitalPin.P11, 1)
+    pins.digitalWritePin(DigitalPin.P9, 0)
+    pins.digitalWritePin(DigitalPin.P10, 1)
     basic.pause(4800)
-    pins.digitalWritePin(DigitalPin.P5, 0)
-    pins.digitalWritePin(DigitalPin.P11, 0)
+    pins.digitalWritePin(DigitalPin.P9, 0)
+    pins.digitalWritePin(DigitalPin.P10, 0)
     basic.pause(500)
     pins.digitalWritePin(DigitalPin.P12, 1)
     pins.digitalWritePin(DigitalPin.P15, 0)
@@ -66,16 +64,17 @@ function ame2 () {
     pins.digitalWritePin(DigitalPin.P12, 0)
     pins.digitalWritePin(DigitalPin.P15, 0)
     basic.pause(500)
-    pins.digitalWritePin(DigitalPin.P5, 1)
-    pins.digitalWritePin(DigitalPin.P11, 0)
+    pins.digitalWritePin(DigitalPin.P9, 1)
+    pins.digitalWritePin(DigitalPin.P10, 0)
     basic.pause(4750)
-    pins.digitalWritePin(DigitalPin.P5, 0)
-    pins.digitalWritePin(DigitalPin.P11, 0)
+    pins.digitalWritePin(DigitalPin.P9, 0)
+    pins.digitalWritePin(DigitalPin.P10, 0)
     isB = 0
 }
 let isB = 0
 isB = 0
 basic.forever(function () {
+    led.enable(false)
     pins.digitalWritePin(DigitalPin.P8, 0)
     pins.digitalWritePin(DigitalPin.P0, 1)
     if (pins.digitalReadPin(DigitalPin.P16) == 1) {
@@ -103,7 +102,7 @@ basic.forever(function () {
     pins.digitalWritePin(DigitalPin.P1, 1)
     if (pins.digitalReadPin(DigitalPin.P16) == 1) {
         basic.showNumber(1)
-        ame1()
+        ame2()
     } else if (pins.digitalReadPin(DigitalPin.P13) == 1) {
         basic.showNumber(2)
         ame2()
