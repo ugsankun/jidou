10b9e5954dd3f65b9caf8fe24e10c4f2f9a449cb
ugsankun <95005131+ugsankun@users.noreply.github.com>
2023-05-01 21:44:00 +0900

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,27 @@
+function ame3 () {
+    isB = 1
+    pins.digitalWritePin(DigitalPin.P9, 0)
+    pins.digitalWritePin(DigitalPin.P10, 1)
+    basic.pause(4900)
+    pins.digitalWritePin(DigitalPin.P9, 0)
+    pins.digitalWritePin(DigitalPin.P10, 0)
+    basic.pause(500)
+    pins.digitalWritePin(DigitalPin.P12, 1)
+    pins.digitalWritePin(DigitalPin.P15, 0)
+    basic.pause(1300)
+    pins.digitalWritePin(DigitalPin.P12, 0)
+    pins.digitalWritePin(DigitalPin.P15, 1)
+    basic.pause(1200)
+    pins.digitalWritePin(DigitalPin.P12, 0)
+    pins.digitalWritePin(DigitalPin.P15, 0)
+    basic.pause(500)
+    pins.digitalWritePin(DigitalPin.P9, 1)
+    pins.digitalWritePin(DigitalPin.P10, 0)
+    basic.pause(4800)
+    pins.digitalWritePin(DigitalPin.P9, 0)
+    pins.digitalWritePin(DigitalPin.P10, 0)
+    isB = 0
+}
 // keypad
 // 
 // pin8
@@ -27,7 +51,7 @@ function ame1 () {
     isB = 1
     pins.digitalWritePin(DigitalPin.P9, 0)
     pins.digitalWritePin(DigitalPin.P10, 1)
-    basic.pause(3600)
+    basic.pause(1000)
     pins.digitalWritePin(DigitalPin.P9, 0)
     pins.digitalWritePin(DigitalPin.P10, 0)
     basic.pause(500)
@@ -42,7 +66,31 @@ function ame1 () {
     basic.pause(500)
     pins.digitalWritePin(DigitalPin.P9, 1)
     pins.digitalWritePin(DigitalPin.P10, 0)
-    basic.pause(3550)
+    basic.pause(1000)
+    pins.digitalWritePin(DigitalPin.P9, 0)
+    pins.digitalWritePin(DigitalPin.P10, 0)
+    isB = 0
+}
+function ame4 () {
+    isB = 1
+    pins.digitalWritePin(DigitalPin.P9, 0)
+    pins.digitalWritePin(DigitalPin.P10, 1)
+    basic.pause(4900)
+    pins.digitalWritePin(DigitalPin.P9, 0)
+    pins.digitalWritePin(DigitalPin.P10, 0)
+    basic.pause(500)
+    pins.digitalWritePin(DigitalPin.P12, 1)
+    pins.digitalWritePin(DigitalPin.P15, 0)
+    basic.pause(1300)
+    pins.digitalWritePin(DigitalPin.P12, 0)
+    pins.digitalWritePin(DigitalPin.P15, 1)
+    basic.pause(1200)
+    pins.digitalWritePin(DigitalPin.P12, 0)
+    pins.digitalWritePin(DigitalPin.P15, 0)
+    basic.pause(500)
+    pins.digitalWritePin(DigitalPin.P9, 1)
+    pins.digitalWritePin(DigitalPin.P10, 0)
+    basic.pause(4800)
     pins.digitalWritePin(DigitalPin.P9, 0)
     pins.digitalWritePin(DigitalPin.P10, 0)
     isB = 0
@@ -51,7 +99,7 @@ function ame2 () {
     isB = 1
     pins.digitalWritePin(DigitalPin.P9, 0)
     pins.digitalWritePin(DigitalPin.P10, 1)
-    basic.pause(4800)
+    basic.pause(4900)
     pins.digitalWritePin(DigitalPin.P9, 0)
     pins.digitalWritePin(DigitalPin.P10, 0)
     basic.pause(500)
@@ -66,27 +114,24 @@ function ame2 () {
     basic.pause(500)
     pins.digitalWritePin(DigitalPin.P9, 1)
     pins.digitalWritePin(DigitalPin.P10, 0)
-    basic.pause(4750)
+    basic.pause(4800)
     pins.digitalWritePin(DigitalPin.P9, 0)
     pins.digitalWritePin(DigitalPin.P10, 0)
     isB = 0
 }
 let isB = 0
 isB = 0
+let ispad = 0
 basic.forever(function () {
     led.enable(false)
     pins.digitalWritePin(DigitalPin.P8, 0)
     pins.digitalWritePin(DigitalPin.P0, 1)
     if (pins.digitalReadPin(DigitalPin.P16) == 1) {
         basic.showString("c")
-        pins.analogWritePin(AnalogPin.P5, 0)
-        pins.analogWritePin(AnalogPin.P11, 500)
     } else if (pins.digitalReadPin(DigitalPin.P13) == 1) {
         basic.showNumber(0)
     } else if (pins.digitalReadPin(DigitalPin.P14) == 1) {
         basic.showString(".")
-        pins.analogWritePin(AnalogPin.P5, 500)
-        pins.analogWritePin(AnalogPin.P11, 0)
     } else {
         basic.showLeds(`
             . . . . .
@@ -95,19 +140,30 @@ basic.forever(function () {
             . . . . .
             . . . . .
             `)
-        pins.analogWritePin(AnalogPin.P5, 0)
-        pins.analogWritePin(AnalogPin.P11, 0)
     }
     pins.digitalWritePin(DigitalPin.P0, 0)
     pins.digitalWritePin(DigitalPin.P1, 1)
     if (pins.digitalReadPin(DigitalPin.P16) == 1) {
+        ispad = 1
         basic.showNumber(1)
-        ame2()
+        music.ringTone(262)
+        ame1()
+        music.stopAllSounds()
+        ispad = 0
     } else if (pins.digitalReadPin(DigitalPin.P13) == 1) {
+        ispad = 1
         basic.showNumber(2)
+        music.ringTone(294)
         ame2()
+        music.stopAllSounds()
+        ispad = 0
     } else if (pins.digitalReadPin(DigitalPin.P14) == 1) {
+        ispad = 1
         basic.showNumber(3)
+        music.ringTone(330)
+        ame3()
+        music.stopAllSounds()
+        ispad = 0
     } else {
         basic.showLeds(`
             . . . . .
@@ -120,7 +176,12 @@ basic.forever(function () {
     pins.digitalWritePin(DigitalPin.P1, 0)
     pins.digitalWritePin(DigitalPin.P2, 1)
     if (pins.digitalReadPin(DigitalPin.P16) == 1) {
+        ispad = 1
         basic.showNumber(4)
+        music.ringTone(330)
+        ame4()
+        music.stopAllSounds()
+        ispad = 0
     } else if (pins.digitalReadPin(DigitalPin.P13) == 1) {
         basic.showNumber(5)
     } else if (pins.digitalReadPin(DigitalPin.P14) == 1) {
@@ -150,5 +211,22 @@ basic.forever(function () {
             . . . . .
             . . . . .
             `)
+    }
+})
+basic.forever(function () {
+    if (ispad == 0) {
+        if (input.buttonIsPressed(Button.A)) {
+            isB = 1
+            pins.analogWritePin(AnalogPin.P9, 0)
+            pins.analogWritePin(AnalogPin.P10, 500)
+        } else if (input.buttonIsPressed(Button.B)) {
+            isB = 1
+            pins.analogWritePin(AnalogPin.P9, 495)
+            pins.analogWritePin(AnalogPin.P10, 0)
+        } else if (isB == 1) {
+            isB = 0
+            pins.analogWritePin(AnalogPin.P9, 0)
+            pins.analogWritePin(AnalogPin.P10, 0)
+        }
     }
 })
